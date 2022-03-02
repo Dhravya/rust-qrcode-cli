62dc431ea0277e08874aef6d9dd344c69facbb3b
Dhravya <dhravyashah@gmail.com>
2022-03-02 20:48:18 +0530

    using a simpler, ligher http client now

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use clap::Parser;
+use std::io::Read;
 use std::io::Write;
-use viuer::{print_from_file, Config};
+use clap::Parser;
 
 // Simple CLI to make QR codes
 #[derive(Parser, Debug)]
@@ -23,37 +23,19 @@ fn main() {
     let args = Cli::parse();
     
     // Using blocking request
-    let res = reqwest::blocking::get(format!("https://api.dhravya.me/qrcode?query={}", args.data).as_str());
-
-    // Check if the request was successful
-    if res.is_err() {
-        println!("Error: {}", res.err().unwrap());
-        return;
-    }
-
-    // Get the response body in bytes
-    let body = res.unwrap().bytes().unwrap();
-
-    // Create a file in the output directory
-    let mut file_ = std::fs::File::create(args.output.join("qrcode.png")).unwrap();
-
-    // Save the body to file
-    file_.write_all(&body).unwrap();
-
+    let mut res = ureq::get(format!("https://api.dhravya.me/qrcode?query={}", args.data).as_str())
+        .call()
+        .unwrap()
+        .into_reader();
     
-    let conf = Config {
-        // set offset
-        x: 4,
-        y: 4,
-        // set dimensions
-        width: Some(80),
-        height: Some(40),
-        ..Default::default()
-    };
+    // Get the image data
+    let mut data = Vec::new();
+    res.read_to_end(&mut data).unwrap();
+
+    // Write the image to the file
+    let mut file = std::fs::File::create(args.output.join("qrcode.png")).unwrap();
+    file.write_all(&data).unwrap();
 
     println!("QR code saved to {}", args.output.join("qrcode.png").display());
 
-    if args.display {
-        print_from_file(args.output.join("qrcode.png").to_str().unwrap(), &conf).expect("Image printing failed.");
-    }
 }
